47799de2a416f6bcf682fad35ce108416b2f8bc8
Antonio Jefferson Moreira Freitas <jeffersonfreitas@unimedce.com.br>
2023-04-21 11:27:09 -0300

    Page home text

--- src/pages/SectionHello.vue
+++ src/pages/SectionHello.vue
@@ -1,7 +1,29 @@
 <template>
-  <div class="w-full h-screen text-sm mt-16">
-    <div class="left-10 h-full bg-red-400">
-      Hello Section
+  <div class="w-full h-screen mt-16">
+    <div class="left-10 h-full bg-black">
+      
+      <div class="grid grid-rows-1 w-full text-5xl font-bold py-16">
+        <h1 class="text-default-green-1">Full-Stack Developer</h1>
+      </div>
+
+      <div class="grid grid-cols-2">
+        <div class="px-16 text-slate-50">
+          <div >
+            <h2 class="text-4xl font-medium">Bem-vindo(a) ao meu Portfólio!</h2>
+          </div>
+  
+          <div class="text-justify">
+            <p class="text-lg font-medium mt-10"><strong class="text-default-green-1">Sou um desenvolvedor de software fullstack apaixonado por criar soluções inovadoras e eficientes para os meus clientes.</strong> Com anos de experiência em desenvolvimento web, estou sempre buscando novas tecnologias e tendências para garantir que minhas soluções stejam sempre atualizadas e atendam às necessidades dos usuários.</p>
+            <p class="text-lg font-medium mt-10">Neste portfólio, você encontrará alguns dos projetos que desenvolvi, tanto em front-end quanto em back-end, utilizando diversas linguagens e ferramentas.  Além disso, você poderá conhecer um pouco mais sobre mim e minhas habilidades técnicas.</p>
+          </div>
+        </div>
+
+        <div class="px-16 text-slate-50">
+          <div class="text-justify">
+            <p class="text-lg font-medium mt-10">Se você estiver procurando por um desenvolvedor fullstack que possa transformar suas ideias em realidade, sinta-se à vontade para entrar em contato comigo. Vamos trabalhar juntos para construir soluções incríveis!</p>
+          </div>
+        </div>
+      </div>
 
     </div>
   </div>
