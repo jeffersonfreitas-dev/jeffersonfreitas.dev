9520b7c6b0e0eecdf6370c182316d273df9b0660
Antonio Jefferson Moreira Freitas <jeffersonfreitas@unimedce.com.br>
2023-05-01 14:08:14 -0300

    Retirado tela branca

--- src/pages/SectionProjects.vue
+++ src/pages/SectionProjects.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="w-full h-screen mt-16">
-    <div class="left-10 h-full bg-white">
+  <div class="w-full h-screen">
+    <div class="left-10 h-full">
       
-      <div class="grid grid-rows-1 w-full text-5xl font-bold">
+      <div class="grid grid-rows-1 w-full text-5xl font-bold py-16">
         <h1 class="text-default-green-1">Projetos</h1>
       </div>
     </div>
