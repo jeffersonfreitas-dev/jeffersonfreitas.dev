4c6cbf29ccabb617a5de0aac3af7795e86a06fd7
Antonio Jefferson Moreira Freitas <jeffersonfreitas@unimedce.com.br>
2023-11-01 20:38:33 -0300

    Pipeline firebase

--- src/pages/SectionHello.vue
+++ src/pages/SectionHello.vue
@@ -18,17 +18,9 @@
               <div class="text-justify">
                 <p class="text-lg font-medium mt-10"><strong class="text-white">Sou um desenvolvedor de software FullCycle apaixonado por criar soluções inovadoras e eficientes para os meus clientes.</strong></p>
                 <p class="text-lg font-medium mt-10">Com anos de experiência em desenvolvimento web, estou sempre buscando novas tecnologias e tendências para garantir que minhas soluções stejam sempre atualizadas e atendam às necessidades dos usuários.</p>
-                <!-- <p class="text-lg font-medium mt-10">Neste portfólio, você encontrará alguns dos projetos que desenvolvi, tanto em front-end quanto em back-end, utilizando diversas linguagens e ferramentas.  Além disso, você poderá conhecer um pouco mais sobre mim e minhas habilidades técnicas.</p> -->
               </div>
               <div class="mt-16">
                 <a class="rounded-xl bg-slate-500 py-4 px-6 hover:bg-slate-700" href="#projects">Meus Projetos</a>
-              </div>
-            </div>
-    
-            <div class="px-16 text-slate-50">
-              <div class="text-justify">
-                <!-- <p class="text-lg font-medium mt-10">Se você estiver procurando por um desenvolvedor fullstack que possa transformar suas ideias em realidade, sinta-se à vontade para entrar em contato comigo. Vamos trabalhar juntos para construir soluções incríveis!</p> -->
-                <div><img src="../assets/hand-touch.jpg" /></div>
               </div>
             </div>
           </div>
