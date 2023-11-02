fa7b683c689b18248f54f778eeca2fd4dbef46d4
Antonio Jefferson Moreira Freitas <jeffersonfreitas@unimedce.com.br>
2023-11-02 17:24:20 -0300

    Finalizado hello desktop

--- src/main.js
+++ src/main.js
@@ -2,9 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import '@/assets/main.css'
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faEnvelope} from "@fortawesome/free-solid-svg-icons";
+import { faGithub,faInstagram, faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-library.add(faBars);
+library.add(faBars, faGithub, faInstagram, faEnvelope, faLinkedinIn);
 
 createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount('#app')
--- src/pages/SectionHello.vue
+++ src/pages/SectionHello.vue
@@ -18,6 +18,34 @@
       
       
       <div class="lg:col-span-1 w-30 h-full flex flex-col justify-center items-center">
+        <ul v-if="!isMobile" class="text-md text-white list-none mt-4 px-16 max-w-prose text-justify">
+          <li
+            class="mb-4"
+            v-for="(item, index) in listItems"
+            :key="index"
+            v-show="showItems[index]"
+          >
+            {{ item }}
+          </li>
+          <br>
+        </ul>
+  
+        
+        <div class="mt-4" v-if="!isMobile">
+          <a href="https://github.com/jeffersonfreitas-dev?tab=repositories" class=" rounded-full p-4 mx-4 bg-default-gold-1">
+            <font-awesome-icon  :icon="['fab', 'github']" size="16px"/>
+          </a>
+          <a href="https://www.linkedin.com/in/jefferson-freitas/" class=" rounded-full p-4 mx-4 bg-default-gold-1">
+            <font-awesome-icon :icon="['fab', 'linkedin-in']" size="16px"/>
+          </a>
+          <a href="#" class=" rounded-full p-4 mx-4 bg-default-gold-1">
+            <font-awesome-icon :icon="['fab', 'instagram']" />
+          </a>
+          <a href="#" class=" rounded-full p-4 mx-4 bg-default-gold-1">
+            <font-awesome-icon :icon="['fas', 'envelope']" />
+          </a>
+        </div>
+
         <blockquote v-if="isMobile" class="text-sm px-12 text-justify">
           Como desenvolvedor de software, <span class="font-bold">estou aqui para criar soluções eficientes e escaláveis</span>. Juntos, podemos transformar desafios em oportunidades. Explore e conheça meu trabalho.
         </blockquote>
@@ -27,6 +55,7 @@
   </div>
 </template>
 
+
 <script>
 
 export default {
@@ -35,13 +64,23 @@ export default {
     return {
       phrases: ["Backend", "Frontend", "Devops"],
       currentPhraseIndex: 0,
-      isMobile: false
+      isMobile: false,
+      listItems: [
+        "Proficiente em Java, JavaScript e amplo conhecimento com Spring Framework (Spring Boot, Security, Cloud ...)",
+        "Experiência com bancos de dados relacionais, como MySQL, Oracle e PostgreSQL, e NoSQL, incluindo MongoDB e Firestore.",
+        "Habilidades em HTML, CSS, JavaScript, Angular, VueJS e ReactJS.",
+        "Experiência com Docker e conhecimentos em mensageria (Apache Kafka e RabbitMQ).",
+        "Compreensão abrangente de redes de computadores, segurança e CI/CD."
+      ],
+      showItems: Array(7).fill(false),
+      currentItemIndex: 0
     };
   },
   mounted() {
     this.displayPhrases();
     this.isMobile = window.innerWidth <= 768;
     window.addEventListener('resize', this.checkIsMobile);
+    this.showNextItem();
   },
   methods: {
     checkIsMobile() {
@@ -53,6 +92,11 @@ export default {
         phrasesElement.textContent = this.phrases[this.currentPhraseIndex];
         this.currentPhraseIndex = (this.currentPhraseIndex + 1) % this.phrases.length;
       }, 1500);
+    },
+    showNextItem() {
+      this.showItems[this.currentItemIndex] = true;
+      this.currentItemIndex = (this.currentItemIndex + 1) % this.listItems.length;
+      setTimeout(this.showNextItem, 1000);
     }
   }
 };
