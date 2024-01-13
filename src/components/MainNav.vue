f60b13c98e10e8176e259fa5046171ab965e2330
Antonio Jefferson Moreira Freitas <jeffersonfreitas@unimedce.com.br>
2024-01-12 21:55:13 -0300

    Tela de sobre

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -1,34 +1,8 @@
 <template>
-  <header class="d-flex justify-content-center">
+  <header class="d-flex justify-content-center ">
     <div class="row container">
-      <div class="col-lg-4 col-md-4 col-sm-10 col-10 d-flex align-items-center">
+      <div class="col-12 d-flex align-items-center justify-content-center">
         <a href="#hello" class="company_name fs-3 fw-bold text-decoration-none">{{ company }}</a>
-      </div>
-      
-      <div class="col-lg-1 col-md-1 col-sm-2 col-2 d-flex flex-row align-items-center justify-content-center">
-        <!-- Botão para acionar o menu em dispositivos móveis -->
-        <button
-        class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarCollapse"
-          aria-controls="navbarCollapse"
-          aria-expanded="false" 
-          aria-label="Toggle navigation"
-          @click="toggleMenu"
-          >
-          <font-awesome-icon :icon="['fas', 'bars']" color="white" class="fa-xl" v-if="isMobile"/>
-        </button>
-      </div>
-      
-      <div class="col-lg-7 col-md-7 col-sm-12 d-flex align-items-center menu">
-        <div class="collapse navbar-collapse" :class="{ show: isMenuOpen }" id="navbarCollapse">
-          <ul class="navbar-nav me-auto mb-2 mb-md-0 list-unstyled d-flex flex-column justify-content-center align-items-start flex-md-row gap-2 menu_container">
-            <li v-for="item in menuItems" :key="item">
-              <a href="#" class="text-decoration-none menu_items">{{ item.name }}</a>
-            </li>
-          </ul>
-        </div>
       </div>
     </div>
   </header>
@@ -39,7 +13,7 @@ export default {
   name: "MainNav",
   data() {
     return {
-      company: "Jefferson Freitas",
+      company: "Portfólio de Jefferson Freitas",
       menuItems: [
         { name: "Sobre", link: "#about" },
         { name: "Projetos", link: "#projects" },
