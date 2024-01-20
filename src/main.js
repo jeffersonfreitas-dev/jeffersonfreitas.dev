5955a211ec416a7dff8740cf1cdd6136f9e9275e
Antonio Jefferson Moreira Freitas <jeffersonfreitas@unimedce.com.br>
2024-01-19 22:06:12 -0300

    Inicioo tela de projetos

--- src/main.js
+++ src/main.js
@@ -3,10 +3,10 @@ import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faBars, faEnvelope, faPhone, faComment, faCode, faHandshake, faAddressCard} from "@fortawesome/free-solid-svg-icons";
+import { faBars, faEnvelope, faPhone, faComment, faCode, faHandshake, faAddressCard, faDiagramProject} from "@fortawesome/free-solid-svg-icons";
 import { faGithub,faInstagram, faLinkedinIn, faWhatsapp } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-library.add(faBars, faGithub, faInstagram, faEnvelope, faPhone, faWhatsapp, faCode, faComment, faLinkedinIn, faHandshake, faAddressCard);
+library.add(faBars, faGithub, faInstagram, faEnvelope, faPhone, faWhatsapp, faCode, faComment, faLinkedinIn, faHandshake, faAddressCard, faDiagramProject);
 
 createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount('#app')
