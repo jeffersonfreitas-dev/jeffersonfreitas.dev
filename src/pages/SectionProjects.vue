af172a23e86653e22c7f5f9f75f6e0b72f16bee0
Antonio Jefferson Moreira Freitas <jeffersonfreitas@unimedce.com.br>
2024-04-23 17:15:44 -0300

    Alterado forma visualização projetos

--- src/pages/SectionProjects.vue
+++ src/pages/SectionProjects.vue
@@ -1,83 +1,83 @@
 <template>
-<div class="container-fluid flex-column text-white"> 
-  <div class="row">
-
-    <div class="d-flex flex-row align-items-center justify-content-center text-white font-monospace py-5">
-      <h1 class="text-center fs-1">Meus Projetos</h1>
-      <div class="text-center">
-        <font-awesome-icon :icon="['fa', 'diagram-project']" class="fa-2x" color="white"/>
-      </div>
+<div class="container-fluid"> 
+  <div class="d-flex flex-row align-items-center justify-content-center text-white font-monospace py-5">
+    <h1 class="text-center fs-1">Meus Projetos</h1>
+    <div class="text-center">
+      <font-awesome-icon :icon="['fa', 'diagram-project']" class="fa-2x" color="white"/>
     </div>
-    
-    <div class="row col-lg-3 col-md-4 col-sm-12 col-xs-12 d-flex">
-      <div class="row text-center justify-content-center align-items-center">
-        <p class="pt-3 fs-3"><strong style="color: #2e8b57;">Simed3 Financeiro</strong></p>
-      </div>
-    </div>
-      
-    <div class="row col-lg-9 col-md-8 col-sm-12 col-xs-12">
-      <div class="d-flex flex-row align-items-center justify-content-center text-white font-monospace">
-        <p >Sistema financeiro para controle de contas a pagar, receber, transferências, extratos bancários, controle de caixa, 
-          relatórios, impostos e muito mais!</p>
-      </div>
-
-      <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-inner rounded-4">
-          <div class="carousel-item active">
-            <img src="../assets/simed/simed01.png" class="d-block w-100" alt="Simed01">
+  </div>
+  
+  <div class="row d-flex justify-content-center">
+    <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12 mb-5" v-for="(p) in projetos" :key="p.name">
+      <div class="card-deck">
+        <div class="card">
+          <img class="card-img-top" :src="`./src/assets/${p.imageFolder}/0.png`" :alt="projetoSelected.name">
+          <div class="card-body">
+            <h5 class="card-title">{{ p.name }}</h5>
+            <p class="card-text">{{ p.description }}</p>
           </div>
-          <div class="carousel-item">
-            <img src="../assets/simed/simed02.png" class="d-block w-100" alt="Simed02">
-          </div>
-          <div class="carousel-item">
-            <img src="../assets/simed/simed03.png" class="d-block w-100" alt="Simed03">
-          </div>
-          <div class="carousel-item">
-            <img src="../assets/simed/simed04.png" class="d-block w-100" alt="Simed04">
-          </div>
-          <div class="carousel-item">
-            <img src="../assets/simed/simed05.png" class="d-block w-100" alt="Simed05">
+          <div class="card-footer">
+            <button class=" btn btn-primary active p-3 rounded-4" style="background-color: #2e8b57; border-color: #2e8b57;" 
+              data-bs-toggle="modal" data-bs-target="#exampleModal" @click="selecionarProjeto(p)">Mais Detalhes
+            </button>
           </div>
         </div>
       </div>
-      <div class="d-flex flex-row align-items-center justify-content-center text-white font-monospace mt-3">
-        <button class=" btn btn-primary active p-3 rounded-4 mb-4" style="background-color: #2e8b57; border-color: #2e8b57;" 
-          data-bs-toggle="modal" data-bs-target="#exampleModal">Mais Detalhes</button>
-      </div>
-    </div>
-
+    </div>  
   </div>   
 
-  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog text-black modal-md">
+  <div class="modal fade text-start" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog text-black modal-lg">
       <div class="modal-content">
         <div class="modal-header">
-          <h1 class="modal-title fs-5" id="exampleModalLabel">Simed3 Financeiro</h1>
+          <h1 class="modal-title fs-5" id="exampleModalLabel">{{ projetoSelected.name }}</h1>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
+        
         <div class="modal-body">
-         <p>Sistema criado para automatizar, controlar e melhorar os processos financeiros de um cliente que realizava todas as rotinas financeiras de forma manual e através de planilhas eletrônicas (Excell).</p>
+          <div class="row d-flex col-12 d-flex justify-content-center align-items-center">
+            <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+              <div class="carousel-inner rounded-4">
+                <div class="carousel-item active">
+                  <img :src="`./src/assets/${projetoSelected.imageFolder}/0.png`" class="d-block w-100" :alt="projetoSelected.name">
+                </div>
+                <div class="carousel-item">
+                  <img :src="`./src/assets/${projetoSelected.imageFolder}/1.png`" class="d-block w-100" :alt="projetoSelected.name">
+                </div>
+                <div class="carousel-item">
+                  <img :src="`./src/assets/${projetoSelected.imageFolder}/2.png`" class="d-block w-100" :alt="projetoSelected.name">
+                </div>
+                <div class="carousel-item">
+                  <img :src="`./src/assets/${projetoSelected.imageFolder}/3.png`" class="d-block w-100" :alt="projetoSelected.name">
+                </div>
+                <div class="carousel-item">
+                  <img :src="`./src/assets/${projetoSelected.imageFolder}/4.png`" class="d-block w-100" :alt="projetoSelected.name">
+                </div>
+              </div>
+            </div>
+          </div>
+
+         <p class="mt-5">{{ projetoSelected.detailDescription }}</p>
+
          
-         <p><strong>Experimente</strong></p>
-         <p>Você pode experimentar uma amostra da aplicação clicando <a target="_blank" class="link-opacity-75 mr-5" href="https://jeffersonfreitas.dev/simed3">aqui</a> ou no link abaixo.</p>
+         <p><strong>Acesso ao Projeto</strong></p>
+         <p v-if="projetoSelected.tests">
+            Você pode conferir o projeto rodando em um ambiente de teste. Para isso, basta clicar no link ao lado <a target="_blank" class="link-opacity-75 mr-5" :href="projetoSelected.link">{{ projetoSelected.name }}</a> e explorar todas as funcionalidades da aplicação. 
+            As credenciais de acesso estão descritas na tela de login, caso existam.</p>
+          
+          <p v-if="projetoSelected.code">
+            Você pode conferir o código fonte do projeto no meu github. Para isso, basta clicar no link ao lado <a target="_blank" class="link-opacity-75 mr-5" href="{{ projetoSelected.link }}">{{ projetoSelected.name }}</a>.</p>
 
          <p><strong>Funcionalidades</strong></p>
-         <p>O Simed3 realiza o controle de contas a pagar, contas a receber, realiza os pagamentos com emissão de ordem para autorização junto ao banco, realiza transferências entre as contas,
-          cadastra empresas, contas bancárias, fornecedores, clientes, movimentações com abertura e fechamento com bloqueio, extratos das movimentações com emissão de 
-          relatórios simplificado e detalhado. Possui opção para realizar upload das notas fiscais das contas a pagar e opção de visualização e impressão das mesmas. 
-         </p>
-         <p>Atualmente o simed3 possui outros módulos solicitados pelos clientes como: Estoque com Solicitações, Pesquisa de Satisfação, Produção/Controles e outros.</p>
-         <p><strong>Tecnologias Usadas no Projeto</strong></p>
-         <ul>
-          <li>Java com Spring Framework</li>
-          <li>MySQL Database</li>
-          <li>Thymeleaf</li>
-          <li>Docker</li>
-         </ul>
+         <p>{{ projetoSelected.functions }}</p>
+
+          <ul class="list-group text-center">
+            <li class="list-group-item active">Tecnologias utilizadas no projeto</li>
+            <li class="list-group-item" v-for="s in stacksItems" :key="s">{{ s }}</li>
+          </ul>
         </div>
         <div class="modal-footer">
-          <a target="_blank" class="link-opacity-75 mr-5" href="https://jeffersonfreitas.dev/simed3">Clique aqui para experimentar</a>
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" style="background-color: #30302a; border-color: #30302a;">Fechar</button>
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" style="background-color: #2e8b57; border-color: #2e8b57;">Fechar</button>
         </div>
       </div>
     </div>
@@ -87,18 +87,28 @@
 
 <script>
 import 'bootstrap/dist/css/bootstrap.min.css';
+import projetos from '../projetos';
 
 export default {
   name: "SectionProjects",
   data() {
     return {
       isMobile: false,
+      projetos: projetos,
+      projetoSelected: projetos[0],
+      stacksItems: []
     };
   },
   mounted() {
     this.isMobile = window.innerWidth <= 768;
     window.addEventListener('resize', this.checkIsMobile);
   },
+  methods: {
+    selecionarProjeto(projeto) {
+      this.projetoSelected = projeto;
+      this.stacksItems = this.projetoSelected.stacks.split(";")
+    }
+  }
 }
 </script>
 
---
+++ src/projetos.js
@@ -0,0 +1,26 @@
+
+export default [
+  {
+    name: "Simed Financeiro",
+    description: "Sistema financeiro feito em Java com Thymeleaf e banco de dados MySQL para controles de contas a pagar, contas a receber, transferências, extratos bancários, controle de caixa, relatórios simplificados e detalhados, impostos e muito mais!",
+    imageFolder: "simed",
+    detailDescription: "Sistema criado para automatizar, controlar e melhorar os processos financeiros de um cliente que realizava todas as rotinas financeiras de forma manual e através de planilhas eletrônicas demandando muito tempo do funcionário do setor ao registrar as movimentações.",
+    link: "https://jeffersonfreitas.dev/simed3",
+    functions: "O Simed3 realiza o controle de contas a pagar, contas a receber, realiza os pagamentos com emissão de ordem para autorização junto ao banco, realiza transferências entre as contas, cadastra empresas, contas bancárias, fornecedores, clientes, movimentações com abertura e fechamento com bloqueio, extratos das movimentações com emissão de relatórios simplificado e detalhado. Possui opção para realizar upload das notas fiscais das contas a pagar e opção de visualização e impressão das mesmas.\n Atualmente o simed3 possui outros módulos solicitados pelos clientes como: Estoque com Solicitações, Pesquisa de Satisfação, Produção/Controles e outros.",
+    stacks: "Java com Spring MVC no Backend; Thymeleaf no Frontend; MySQL Database; Ambiente Docker em produção",
+    tests: true,
+    code: false
+  },
+  // {
+  //   name: "MotoGestor 1.0",
+  //   description: "Sistema para gerenciamento de oficina mecânica feito em Java com VueJS e banco de dados PostgreSQL para controles de clientes, veículos, produtos e serviços, ordens de serviços, atendimentos e muito mais! (Em breve módulo financeiro)",
+  //   imageFolder: "motogestor",
+  //   detailDescription: "Projeto solicitado pelo cliente após passar por problemas com históricos dos atendimentos, consultas de informações dos clientes e ordens de serviços ineficientes pois, controlava tudo com planilhas eletrônicas e principalmente tornando o registro das movimentações da oficina demorado e inconsistente. Foram realizados todos os levantamentos dos requisitos e o projeto ainda está em andamento.",
+  //   link: "https://motogestor.jeffersonfreitas.dev/",
+  //   functions: "O Motogestor realiza o cadastro dos clientes e seus veículos. Possui o cadastro de peças e serviços com quantidades e preços, realiza o cadastro da ordem de serviço com busca de clientes e seleção do veículo para manutenção. Possui o atendimento da ordem de serviço com a inclusão das peças e serviços, quantidades e valores bem como o fechamento da ordem com opção de descontos e acréscimos. Módulo financeiro em desenvolvimento, mas possui o registro dos fechamentos das ordens de serviços no livro caixa.\n Possui o cadastro de mecânicos e marcas dos veículos além de um dashboard com graficos e principais indicadores para análise.",
+  //   stacks: "Java com Spring Boot no Backend; VueJS no Frontend; PostgreSQL Database; Ambiente Docker em produção; Autenticação com Firebase",
+  //   tests: true,
+  //   code: false
+  // }
+]
+
