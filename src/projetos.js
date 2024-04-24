c225a81f8762722568fc7822dc3e42ebca5da0ed
Antonio Jefferson Moreira Freitas <jeffersonfreitas@unimedce.com.br>
2024-04-24 16:48:29 -0300

    Adicionando motogestor

--- src/projetos.js
+++ src/projetos.js
@@ -2,7 +2,7 @@
 export default [
   {
     name: "Simed Financeiro",
-    description: "Sistema financeiro feito em Java com Thymeleaf e banco de dados MySQL para controles de contas a pagar, contas a receber, transferências, extratos bancários, controle de caixa, relatórios simplificados e detalhados, impostos e muito mais!",
+    description: "Sistema financeiro feito em Java com Thymeleaf e banco de dados MySQL. Realiza o controle de contas a pagar, contas a receber, transferências, extratos bancários, controle de caixa, relatórios simplificados e detalhados, impostos e muito mais!",
     imageFolder: "simed",
     detailDescription: "Sistema criado para automatizar, controlar e melhorar os processos financeiros de um cliente que realizava todas as rotinas financeiras de forma manual e através de planilhas eletrônicas demandando muito tempo do funcionário do setor ao registrar as movimentações.",
     link: "https://jeffersonfreitas.dev/simed3",
@@ -13,7 +13,7 @@ export default [
   },
   {
     name: "MotoGestor 1.0",
-    description: "Sistema para gerenciamento de oficina mecânica feito em Java com VueJS e banco de dados PostgreSQL para controles de clientes, veículos, produtos e serviços, ordens de serviços, atendimentos e muito mais! (Em breve módulo financeiro)",
+    description: "Sistema para gerenciamento de oficina mecânica feito em Java com VueJS e banco de dados PostgreSQL. Realiza o controle de clientes, veículos, produtos e serviços, ordens de serviços, atendimentos e muito mais! (Em breve módulo financeiro)",
     imageFolder: "motogestor",
     detailDescription: "Projeto solicitado pelo cliente após passar por problemas com históricos dos atendimentos, consultas de informações dos clientes e ordens de serviços ineficientes pois, controlava tudo com planilhas eletrônicas e principalmente tornando o registro das movimentações da oficina demorado e inconsistente. Foram realizados todos os levantamentos dos requisitos e o projeto ainda está em andamento.",
     link: "https://motogestor.jeffersonfreitas.dev/",
